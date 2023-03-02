64fb804129ab5c1e7055977ff1b85db327588df5
Artem Gladkov <artemgladkovvvv@gmail.com>
2023-03-02 20:48:22 +0100

    [BUG FIX]

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch';
+const api_key = '8ef856b0ae6f808f231b0014a93dfaa8'; //cambia el api_key por otro que te sirva, pero tarda dos horas en activarse
+
+console.log("Ciudad: "); //Introucción de ciudad
+process.stdin.addListener("data", async (location) => {
+    const city = location.toString().trim(); //
+    console.log("Tu ciudad es: " + city);
+    const weather = await getWeather(city);
+    console.log(weather);
+    process.exit();
+})
+
+async function getWeather(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`);
+    const data = await response.json(); //Te responde en formato JSON con la información del tiempo
+    return data;
+}
