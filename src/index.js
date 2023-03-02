1cb5dc8307549de9a6eb44afe7f6fa4b3bd2d140
astrosinifinitos <stroncio1981@gmail.com>
2023-03-02 21:21:59 +0100

    Ultimate changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
-const api_key = ''; //cambia el api_key por otro que te sirva, pero tarda dos horas en activarse
+const api_key = '8ef856b0ae6f808f231b0014a93dfaa8'; //cambia el api_key por otro que te sirva, pero tarda dos horas en activarse
 
 console.log("Ciudad: "); //Introucción de ciudad
 process.stdin.addListener("data", async (location) => {
-  const city = location.toString().trim(); //
+  const city = location.toString().trim();
   console.log("Tu ciudad es: " + city);
   const weather = await getWeather(city);
   console.log(weather);
@@ -15,3 +15,6 @@ async function getWeather(city) {
   const data = await response.json(); //Te responde en formato JSON con la información del tiempo
   return data;
 }
+
+const miDiv = document.getElementsByClassName("temp-value");
+miDiv.innerHTML = data;
